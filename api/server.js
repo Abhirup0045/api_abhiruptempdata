ebd1bda11fece740052bf13a47fda3be28b1b654
Abhirup 2.0 <mentalrockeyroy@gmail.com>
2025-09-12 22:31:28 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -5,12 +5,12 @@ const app = express();
 
 // GET all languages
 app.get("/api/lang", (req, res) => {
-  res.json(data.languages);
+  res.json(data.products);
 });
 
 // GET one language by id
 app.get("/api/lang/:id", (req, res) => {
-  const lang = data.languages.find(l => l.id === parseInt(req.params.id));
+  const lang = data.products.find(l => l.id === parseInt(req.params.id));
   lang ? res.json(lang) : res.status(404).json({ error: "Not found" });
 });
 
